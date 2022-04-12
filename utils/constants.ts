eaae6e1f7d6af02104f36648a532a00a086a3897
nemusonaneko <nemusonaneko@protonmail.com>
2022-04-12 15:50:01 -0500

    update constant to add avax mainnet

--- utils/constants.ts
+++ utils/constants.ts
@@ -4,6 +4,7 @@ import { Chain, allChains } from 'wagmi';
 export const FACTORY_RINKEBY = '0xEDF04002c8bDab6AdC2BD738F4e84953bb38c481';
 export const FACTORY_KOVAN = '0x7AaCc52c41DA1Ac634f1e0F1dbefFB593Bc64503';
 export const FACTORY_FUJI = '0xf0CCCd4aD7B92d038E80818C8A85d8D926cf8139';
+export const FACTORY_AVALANCHE = '0xedf04002c8bdab6adc2bd738f4e84953bb38c481';
 
 export const infuraId = 'c580a3487b1241a09f9e27b02c004f5b';
 export const alchemyId = 'PwvZx2hO2XpToWXSw9sgJJt1eBgjkRUr';
@@ -65,10 +66,23 @@ export const networkDetails: INetworkDetails = {
     blockExplorerURL: 'https://testnet.snowtrace.io/',
     blockExplorerName: 'Snowtrace',
   },
+  43114: {
+    rpcUrl: 'https://api.avax.network/ext/bc/C/rpc',
+    rpcProvider: new ethers.providers.JsonRpcProvider('https://api.avax.network/ext/bc/C/rpc'),
+    subgraphEndpoint: 'https://api.thegraph.com/subgraphs/name/nemusonaneko/llamapay-avax-mainnet',
+    chainProviders: new ethers.providers.JsonRpcProvider('https://api.avax.network/ext/bc/C/rpc'),
+    llamapayFactoryAddress: FACTORY_AVALANCHE,
+    blockExplorerURL: 'https://snowtrace.io/',
+    blockExplorerName: 'Snowtrace',
+  },
 };
 
 export const chains: Chain[] = allChains.filter(
-  (chain) => chain.name === 'Rinkeby' || chain.name === 'Kovan' || chain.name === 'Avalanche Fuji Testnet'
+  (chain) =>
+    chain.name === 'Rinkeby' ||
+    chain.name === 'Kovan' ||
+    chain.name === 'Avalanche Fuji Testnet' ||
+    chain.name === 'Avalanche'
 );
 
 export const secondsByDuration = {
