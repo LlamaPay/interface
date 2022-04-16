a174459116a5cbec68bd1ce897f6076293d75d6e
nemusonaneko <nemusonaneko@protonmail.com>
2022-04-16 17:14:10 -0500

    rearranged history and cancel button

--- components/Stream/Table/StreamActions.tsx
+++ components/Stream/Table/StreamActions.tsx
@@ -12,21 +12,27 @@ const StreamActions = ({ data }: { data: IStream }) => {
   return (
     <span className="relative flex justify-end gap-10">
       {isIncoming ? (
-        <Push buttonName="Withdraw" data={data} />
+        <>
+          <Push buttonName="Withdraw" data={data} />
+          <button className="row-action-links" onClick={historyDialog.toggle}>
+            History
+          </button>
+          <StreamHistory data={data} title="Stream History" dialog={historyDialog} />
+        </>
       ) : (
         <>
           <Push buttonName="Send" data={data} />
           <button className="row-action-links" onClick={modifyDialog.toggle}>
             Modify
           </button>
+          <button className="row-action-links" onClick={historyDialog.toggle}>
+            History
+          </button>
+          <StreamHistory data={data} title="Stream History" dialog={historyDialog} />
           <Cancel data={data} />
           <Modify data={data} title="Modify" dialog={modifyDialog} />
         </>
       )}
-      <button className="row-action-links" onClick={historyDialog.toggle}>
-        History
-      </button>
-      <StreamHistory data={data} title="Stream History" dialog={historyDialog} />
     </span>
   );
 };
