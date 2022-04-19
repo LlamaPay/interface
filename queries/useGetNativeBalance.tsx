5cafca031e0e1fdfdc3c91482435518c8c7af595
nemusonaneko <nemusonaneko@protonmail.com>
2022-04-19 00:05:15 -0500

    add native currency

--- queries/useGetNativeBalance.tsx
+++ queries/useGetNativeBalance.tsx
@@ -1,11 +1,13 @@
-import { useBalance } from 'wagmi';
+import { useBalance, useNetwork } from 'wagmi';
 
 export function useGetNativeBalance(id: string) {
   const [{ data, error, loading }] = useBalance({
     addressOrName: id.toLowerCase(),
   });
+  const [{ data: network }] = useNetwork();
+  const nativeCoin = network.chain?.nativeCurrency?.symbol;
 
   if (error) return <p>Failed to Get Balance</p>;
   if (loading) return <p>Loading Balance</p>;
-  return <p>{(Number(data?.value) / 1e18).toLocaleString('en-US', { maximumFractionDigits: 5 })}</p>;
+  return <p>{`${(Number(data?.value) / 1e18).toLocaleString('en-US', { maximumFractionDigits: 5 })} ${nativeCoin}`}</p>;
 }
