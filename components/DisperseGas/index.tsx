a2788fe97396c1382775e3430530b08e7fe6ca60
nemusonaneko <nemusonaneko@protonmail.com>
2022-04-20 13:30:04 -0500

    remake disperse form and add in shortnames

--- components/DisperseGas/index.tsx
+++ components/DisperseGas/index.tsx
@@ -1,8 +1,6 @@
-import { Tab } from '@headlessui/react';
 import { useDialogState } from 'ariakit';
 import { FormDialog } from 'components/Dialog';
 import React from 'react';
-import DisperseForm from './DisperseForm';
 import { useAccount } from 'wagmi';
 import { useNetworkProvider } from 'hooks';
 import SendToPayees from './SendToPayees';
@@ -19,37 +17,12 @@ function DisperseGasMoney() {
       </button>
       <FormDialog
         dialog={disperseDialog}
-        title={` Disperse ${nativeCurrency?.symbol} to Users`}
+        title={` Disperse ${nativeCurrency?.symbol} to Your Payees`}
         className="v-min h-min"
       >
         <div className="space-y-3">
           {accountData && !unsupported ? (
-            <Tab.Group>
-              <Tab.List className="flex space-x-3">
-                <Tab
-                  className={({ selected }) =>
-                    selected ? 'rounded-3xl bg-[#23BD8F] px-4 py-2' : 'rounded-3xl bg-[#ffffff] px-4 py-2'
-                  }
-                >
-                  <span className="font-inter ">Send to Payees</span>
-                </Tab>
-                <Tab
-                  className={({ selected }) =>
-                    selected ? 'rounded-3xl bg-[#23BD8F] px-4 py-2' : 'rounded-3xl bg-[#ffffff] px-4 py-2'
-                  }
-                >
-                  <span className="font-inter ">Disperse Tokens</span>
-                </Tab>
-              </Tab.List>
-              <Tab.Panels>
-                <Tab.Panel>
-                  <SendToPayees />
-                </Tab.Panel>
-                <Tab.Panel>
-                  <DisperseForm />
-                </Tab.Panel>
-              </Tab.Panels>
-            </Tab.Group>
+            <SendToPayees />
           ) : (
             <>
               {!accountData ? <p>Connect Wallet</p> : <></>}
