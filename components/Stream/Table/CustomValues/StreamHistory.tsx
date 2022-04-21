d6bfc452e4fd3bec847c56af11cc6cc825a60e88
nemusonaneko <nemusonaneko@protonmail.com>
2022-04-21 16:40:22 -0500

    modify info shown in modify and streamhistory

--- components/Stream/Table/CustomValues/StreamHistory.tsx
+++ components/Stream/Table/CustomValues/StreamHistory.tsx
@@ -18,11 +18,17 @@ export const StreamHistory = ({ data, dialog, title }: StreamHistoryProps) => {
       <FormDialog dialog={dialog} title={title} className="v-min h-min">
         <section>
           <table className=" w-full border-collapse">
+            <thead>
+              <tr>
+                <th className="text-sm">Event Type</th>
+                <th className="text-sm">Timestamp</th>
+              </tr>
+            </thead>
             <tbody>
               {historicalData.map((p) => (
                 <tr className="border border-stone-700" key={p.txHash}>
-                  <td className="border text-center text-sm">{p.eventType.replace(/([A-Z])/g, ' $1')}</td>
-                  <td className="border text-center text-sm">
+                  <td className="border px-3 text-left text-sm">{p.eventType.replace(/([A-Z])/g, ' $1')}</td>
+                  <td className="border px-3 text-left text-sm">
                     {new Date(Number(p.createdTimestamp) * 1e3).toLocaleString('en-CA', { hour12: false })}
                   </td>
                   <td className="border text-center text-sm">
