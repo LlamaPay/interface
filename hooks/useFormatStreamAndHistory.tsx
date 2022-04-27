155767a2f38b413040599d4190db5879435e1724
nemusonaneko <nemusonaneko@protonmail.com>
2022-04-27 17:50:17 -0500

    idk anymore

--- hooks/useFormatStreamAndHistory.tsx
+++ hooks/useFormatStreamAndHistory.tsx
@@ -47,7 +47,7 @@ export function useFormatStreamAndHistory({
           paused: s.paused,
           pausedAmount: s.pausedAmount,
           lastPaused: s.lastPaused,
-          reason: s.reason ? s.reason : null,
+          reason: s.reason ?? s.reason,
         };
       });
 
