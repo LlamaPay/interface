7501c9e43c51a8422d846076e612396c8d85fc52
nemusonaneko <nemusonaneko@protonmail.com>
2022-04-27 20:08:46 -0500

    handle cancelling paused stream

--- components/Stream/Table/CustomValues/Cancel.tsx
+++ components/Stream/Table/CustomValues/Cancel.tsx
@@ -1,4 +1,6 @@
 import llamaContract from 'abis/llamaContract';
+import { Interface } from 'ethers/lib/utils';
+import useBatchCalls from 'queries/useBatchCalls';
 import * as React from 'react';
 import toast from 'react-hot-toast';
 import { useQueryClient } from 'react-query';
@@ -8,6 +10,9 @@ import { useContractWrite } from 'wagmi';
 interface CancelProps {
   data: IStream;
 }
+
+const CreateInterface = new Interface(['function createStream(address to, uint216 amountPerSec)']);
+const CancelInterface = new Interface(['function cancelStream(address to, uint216 amountPerSec)']);
 
 export const Cancel = ({ data }: CancelProps) => {
   const [{}, cancel] = useContractWrite(
@@ -22,16 +27,27 @@ export const Cancel = ({ data }: CancelProps) => {
   );
 
   const queryClient = useQueryClient();
+  const { mutate: batchCall } = useBatchCalls();
 
   const handleClick = () => {
-    cancel().then((data) => {
-      const loadingToast = data.error ? toast.error(data.error.message) : toast.loading('Cancelling Stream');
-      data.data?.wait().then((receipt) => {
-        toast.dismiss(loadingToast);
-        receipt.status === 1 ? toast.success('Stream Cancelled') : toast.error('Failed to Cancel Stream');
-        queryClient.invalidateQueries();
+    if (data.paused) {
+      batchCall({
+        llamaContractAddress: data.llamaContractAddress,
+        calls: [
+          CreateInterface.encodeFunctionData('createStream', [data.payeeAddress, data.amountPerSec]),
+          CancelInterface.encodeFunctionData('cancelStream', [data.payeeAddress, data.amountPerSec]),
+        ],
       });
-    });
+    } else {
+      cancel().then((data) => {
+        const loadingToast = data.error ? toast.error(data.error.message) : toast.loading('Cancelling Stream');
+        data.data?.wait().then((receipt) => {
+          toast.dismiss(loadingToast);
+          receipt.status === 1 ? toast.success('Stream Cancelled') : toast.error('Failed to Cancel Stream');
+          queryClient.invalidateQueries();
+        });
+      });
+    }
   };
 
   return (
