92c8d6532a0e0a206561d9a8d01c785cdebe3f86
nemusonaneko <nemusonaneko@protonmail.com>
2022-04-29 02:10:21 -0500

    remove detail button if deposit or withdraw

--- components/History/Table/HistoryActions.tsx
+++ components/History/Table/HistoryActions.tsx
@@ -5,14 +5,18 @@ import { MoreInfo } from '../MoreInfo';
 const HistoryActions = ({ data }: { data: IHistory }) => {
   const dialog = useDialogState();
 
-  return (
-    <span className="flex">
-      <button className="row-action-links ml-auto" onClick={dialog.toggle}>
-        Details
-      </button>
-      {data.stream ? <MoreInfo data={data} dialog={dialog} /> : ''}
-    </span>
-  );
+  if (data.eventType !== 'Deposit' && data.eventType !== 'Withdraw') {
+    return (
+      <>
+        <span className="flex">
+          <button className="row-action-links ml-auto" onClick={dialog.toggle}>
+            Details
+          </button>
+        </span>
+        {data.stream ? <MoreInfo data={data} dialog={dialog} /> : ''}
+      </>
+    );
+  }
 };
 
 export default HistoryActions;
