6548be38cbf44f6105d6daa6cb3376783e73a55c
mintdart <96025197+mintdart@users.noreply.github.com>
2022-04-29 06:17:58 +0100

    add testnets to menu

--- components/Web3/NetworksMenu.tsx
+++ components/Web3/NetworksMenu.tsx
@@ -1,5 +1,14 @@
 import * as React from 'react';
-import { Select, SelectItem, SelectLabel, SelectPopover, useSelectState } from 'ariakit/select';
+import {
+  Select,
+  SelectItem,
+  SelectLabel,
+  SelectPopover,
+  useSelectState,
+  SelectGroup,
+  SelectGroupLabel,
+  SelectSeparator,
+} from 'ariakit/select';
 import { SelectorIcon } from '@heroicons/react/solid';
 import { useNetwork } from 'wagmi';
 import { chainDetails } from 'utils/network';
@@ -22,7 +31,7 @@ export const NetworksMenu = () => {
   if (!data || !chain || !switchNetwork) return null;
 
   const mainnets = data.chains.filter((chain) => !chain.testnet);
-  // const testnets = data.chains.filter((chain) => chain.testnet);
+  const testnets = data.chains.filter((chain) => chain.testnet);
 
   return (
     <>
@@ -47,30 +56,59 @@ export const NetworksMenu = () => {
       {select.mounted && (
         <SelectPopover
           state={select}
-          className="shadow-2 z-10 w-fit min-w-[10rem] rounded-xl border border-[#EAEAEA] bg-white p-2"
+          className="shadow-2 z-10 max-h-[280px] w-fit min-w-[12rem] overflow-y-auto rounded-xl border border-[#EAEAEA] bg-white p-2"
         >
-          {mainnets.map((value) => {
-            const { network } = chainDetails(value?.id?.toString());
-            return (
-              <SelectItem
-                key={value.id}
-                value={value.id?.toString()}
-                className="flex cursor-pointer scroll-m-2 items-center gap-4 whitespace-nowrap p-2 font-normal text-[#666666] outline-none active-item:text-black active:text-black aria-disabled:opacity-40"
-                onClick={() => switchNetwork(value.id)}
-              >
-                <div className="flex h-5 w-5 items-center rounded-full">
-                  <Image
-                    src={network.logoURI || defaultImage}
-                    alt={'Logo of ' + value.name}
-                    objectFit="contain"
-                    width="20px"
-                    height="20px"
-                  />
-                </div>
-                <span>{value.name}</span>
-              </SelectItem>
-            );
-          })}
+          <SelectGroup>
+            <SelectGroupLabel className="p-2 text-sm font-normal text-neutral-500">Mainnets</SelectGroupLabel>
+            {mainnets.map((value) => {
+              const { network } = chainDetails(value?.id?.toString());
+              return (
+                <SelectItem
+                  key={value.id}
+                  value={value.id?.toString()}
+                  className="flex cursor-pointer scroll-m-2 items-center gap-4 whitespace-nowrap p-2 font-normal text-[#666666] outline-none active-item:text-black active:text-black aria-disabled:opacity-40"
+                  onClick={() => switchNetwork(value.id)}
+                >
+                  <div className="flex h-5 w-5 items-center rounded-full">
+                    <Image
+                      src={network.logoURI || defaultImage}
+                      alt={'Logo of ' + value.name}
+                      objectFit="contain"
+                      width="20px"
+                      height="20px"
+                    />
+                  </div>
+                  <span>{value.name}</span>
+                </SelectItem>
+              );
+            })}
+          </SelectGroup>
+          <SelectSeparator className="my-2" />
+          <SelectGroup>
+            <SelectGroupLabel className="p-2 text-sm font-normal text-neutral-500">Testnets</SelectGroupLabel>
+            {testnets.map((value) => {
+              const { network } = chainDetails(value?.id?.toString());
+              return (
+                <SelectItem
+                  key={value.id}
+                  value={value.id?.toString()}
+                  className="flex cursor-pointer scroll-m-2 items-center gap-4 whitespace-nowrap p-2 font-normal text-[#666666] outline-none active-item:text-black active:text-black aria-disabled:opacity-40"
+                  onClick={() => switchNetwork(value.id)}
+                >
+                  <div className="flex h-5 w-5 items-center rounded-full">
+                    <Image
+                      src={network.logoURI || defaultImage}
+                      alt={'Logo of ' + value.name}
+                      objectFit="contain"
+                      width="20px"
+                      height="20px"
+                    />
+                  </div>
+                  <span>{value.name}</span>
+                </SelectItem>
+              );
+            })}
+          </SelectGroup>
         </SelectPopover>
       )}
     </>
