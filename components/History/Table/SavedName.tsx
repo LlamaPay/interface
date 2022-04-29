baf0247b45bded4352a4b4d819532a9efb125e12
nemusonaneko <nemusonaneko@protonmail.com>
2022-04-29 17:21:03 -0500

    add etherscan link for address/name in history table

--- components/History/Table/SavedName.tsx
+++ components/History/Table/SavedName.tsx
@@ -1,4 +1,5 @@
 import Tooltip from 'components/Tooltip';
+import { useChainExplorer } from 'hooks';
 import * as React from 'react';
 import { useAddressStore } from 'store/address';
 import { formatAddress } from 'utils/address';
@@ -7,6 +8,13 @@ export function SavedName({ value }: { value: string }) {
   const name =
     useAddressStore((state) => state.addressBook.find((p) => p.id?.toLowerCase() === value?.toLowerCase()))
       ?.shortName ?? formatAddress(value);
+  const { url: chainExplorer } = useChainExplorer();
 
-  return <Tooltip content={value}>{name}</Tooltip>;
+  return (
+    <Tooltip content={value}>
+      <a href={`${chainExplorer}/address/${value}`} target="_blank" rel="noopener noreferrer">
+        {name}
+      </a>
+    </Tooltip>
+  );
 }
