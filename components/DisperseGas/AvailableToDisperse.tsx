eb65e054aedd899bf091336622c03831fa52a05e
mintdart <96025197+mintdart@users.noreply.github.com>
2022-04-30 05:58:14 +0100

    refetch user balance after disperse

--- components/DisperseGas/AvailableToDisperse.tsx
+++ components/DisperseGas/AvailableToDisperse.tsx
@@ -1,39 +1,43 @@
 import Image from 'next/image';
 import { chainDetails } from 'utils/network';
-import { useBalance, useNetwork } from 'wagmi';
+import { useNetwork } from 'wagmi';
 import defaultImage from 'public/empty-token.webp';
 import { useLocale } from 'hooks';
+import { useGetNativeBalance } from 'queries/useGetNativeBalance';
 
 export default function AvailableToDisperse({ id }: { id: string }) {
-  const [{ data: balance, loading }] = useBalance({
-    addressOrName: id.toLowerCase(),
-  });
+  const [{ data: networkData, loading: networkLoading }] = useNetwork();
 
-  const [{ data, loading: networkLoading }] = useNetwork();
+  const { data: nativeBalance, isLoading } = useGetNativeBalance(id);
 
-  const { network } = chainDetails(data?.chain?.id.toString() ?? '0');
+  const { network } = chainDetails(networkData?.chain?.id.toString() ?? '0');
 
   const { locale } = useLocale();
+
+  const balance =
+    nativeBalance &&
+    networkData?.chain &&
+    Number(nativeBalance) / 10 ** Number(networkData?.chain?.nativeCurrency?.decimals);
 
   return (
     <div className="mt-[5px] flex flex-wrap items-center justify-between gap-4 rounded bg-[#E7E7E7]/40 px-2 py-1 text-xs text-[#4E575F]">
       <span>Available To Disperse</span>
-      {networkLoading || loading ? (
+      {isLoading || networkLoading ? (
         <div></div>
       ) : (
         <div className="flex items-center gap-1">
           <div className="flex h-[14px] w-[14px] flex-shrink-0 items-center rounded-full">
             <Image
               src={network?.logoURI ?? defaultImage}
-              alt={'Logo of token ' + data?.chain?.name ?? 'Unsupported Token'}
+              alt={'Logo of token ' + networkData?.chain?.name ?? 'Unsupported Token'}
               width="14px"
               height="14px"
             />
           </div>
           <p>
             {balance &&
-              `${(Number(balance?.value) / 1e18).toLocaleString(locale, { maximumFractionDigits: 5 })} ${
-                balance?.symbol
+              `${balance.toLocaleString(locale, { maximumFractionDigits: 5 })} ${
+                networkData?.chain?.nativeCurrency?.symbol
               }`}
           </p>
         </div>
