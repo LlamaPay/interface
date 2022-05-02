2aba83d65b880da85c4161e3fc34a354d54a00e5
nemusonaneko <nemusonaneko@protonmail.com>
2022-05-01 23:22:34 -0500

    didn't work

--- components/Stream/index.tsx
+++ components/Stream/index.tsx
@@ -7,7 +7,6 @@ import { StreamTable, DefaultStreamTable } from './Table';
 import { IStreamAndHistory } from 'types';
 import StreamMenu from './Menu';
 import { useTranslations } from 'next-intl';
-import TestButton from 'components/TestButton';
 
 export function StreamSection() {
   const { data, isLoading, error } = useStreamsAndHistory();
@@ -26,7 +25,6 @@ export function StreamSection() {
           <Link href="/create">
             <a className="primary-button py-2 px-8 text-sm font-bold">{t('create')}</a>
           </Link>
-          <TestButton />
           <StreamMenu />
         </div>
       </div>
