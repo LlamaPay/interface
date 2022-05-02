ebb0e081af3909b4186c75bc1ba4e44c4ebf50d5
nemusonaneko <nemusonaneko@protonmail.com>
2022-05-02 14:23:15 -0500

    remove unused dependency

--- components/Web3/WalletProvider.tsx
+++ components/Web3/WalletProvider.tsx
@@ -5,7 +5,6 @@ import { Connector, Provider, chain } from 'wagmi';
 import { InjectedConnector } from 'wagmi/connectors/injected';
 import { WalletConnectConnector } from 'wagmi/connectors/walletConnect';
 import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet';
-import SafeProvider from '@gnosis.pm/safe-apps-react-sdk';
 
 const defaultChain = chain.avalanche;
 
