75e401fd9fa0f2f4c814094f257ea133d14c7e38
mintdart <96025197+mintdart@users.noreply.github.com>
2022-05-07 03:51:41 +0100

    refactor

--- components/Web3/WalletSelector.tsx
+++ components/Web3/WalletSelector.tsx
@@ -29,15 +29,15 @@ export const WalletSelector = ({ dialog }: Props) => {
     [connect, dialog]
   );
 
-  React.useEffect(()=>{
-    if(process.env.NEXT_PUBLIC_SAFE === 'true' && typeof window !== 'undefined'){
+  React.useEffect(() => {
+    if (process.env.NEXT_PUBLIC_SAFE === 'true' && typeof window !== 'undefined') {
       connect(connectors[0]);
     }
-  }, [connect, connectors])
+  }, [connect, connectors]);
 
   const formattedAddress = accountData && formatAddress(accountData.address);
 
-  const t = useTranslations('Common')
+  const t = useTranslations('Common');
 
   return (
     <Dialog state={dialog} className="dialog">
@@ -46,20 +46,20 @@ export const WalletSelector = ({ dialog }: Props) => {
           <DialogHeading className="text-base font-medium leading-6 text-neutral-700 dark:text-neutral-200">
             <span>{t('account')}</span>
             <button
-              className="absolute top-6 right-4 rounded hover:bg-neutral-200 dark:hover:bg-zinc-800"
+              className="absolute top-[18px] right-4 rounded hover:bg-neutral-200 dark:hover:bg-zinc-800"
               onClick={dialog.toggle}
             >
               <span className="sr-only">{t('close')}</span>
               <XIcon className="h-5 w-5" />
             </button>
           </DialogHeading>
-          <div className="mt-3 flex flex-col space-y-2">
+          <div className="mt-3 flex flex-col gap-2">
             <p className="text-sm font-thin">{`${t('connectedWith')} ${accountData.connector?.name}`}</p>
             <p className="break-words">
               {accountData.ens?.name ? `${accountData.ens?.name} (${formattedAddress})` : accountData.address}
             </p>
             <button
-              className="nav-button"
+              className="nav-button mt-5"
               onClick={() => {
                 disconnect();
                 dialog.toggle();
@@ -74,14 +74,14 @@ export const WalletSelector = ({ dialog }: Props) => {
           <DialogHeading className="text-base font-medium leading-6 text-neutral-700 dark:text-neutral-200">
             <span>{t('connectWallet')}</span>
             <button
-              className="absolute top-6 right-4 rounded hover:bg-neutral-200 dark:hover:bg-zinc-800"
+              className="absolute top-[18px] right-4 rounded hover:bg-neutral-200 dark:hover:bg-zinc-800"
               onClick={dialog.toggle}
             >
               <span className="sr-only">{t('close')}</span>
               <XIcon className="h-5 w-5" />
             </button>
           </DialogHeading>
-          <div className="mt-3 flex flex-col space-y-2">
+          <div className="mt-3 flex flex-col gap-2">
             {connectors.map((x) => (
               <button key={x.id} onClick={() => handleConnect(x)} className="rounded border p-2">
                 {isMounted ? x.name : x.id === 'injected' ? x.id : x.name}
