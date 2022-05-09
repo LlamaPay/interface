d524e4d391affdae99a1eff402fefa8335b92d1d
mintdart <96025197+mintdart@users.noreply.github.com>
2022-05-09 05:51:25 +0100

    refactor

--- components/StaleSubgraphWarning/index.tsx
+++ components/StaleSubgraphWarning/index.tsx
@@ -2,20 +2,14 @@ import useGetSubgraphDelay from 'queries/useGetSubgraphDelay';
 import React from 'react';
 
 export default function StaleSubgraphWarning() {
-  const [delayTime, setDelayTime] = React.useState<number | null | undefined>(null);
-  const [delayBlocks, setDelayBlocks] = React.useState<number | null | undefined>(null);
   const { data } = useGetSubgraphDelay();
 
-  React.useEffect(() => {
-    setDelayTime(data?.timeDelay);
-    setDelayBlocks(data?.blockDelay);
-  }, [data]);
+  if (!data || data?.timeDelay < 60) return null;
 
-  if (!delayTime || !delayBlocks || delayTime < 60) return <></>;
   return (
     <section className="h-[25px] bg-red-600 text-center text-white">
       <span className="font-exo text-sm">
-        {`Subgraph is currently delayed by ${delayBlocks} blocks (${(delayTime / 60).toFixed(2)} minutes)`}
+        {`Subgraph is currently delayed by ${data.blockDelay} blocks (${(data.timeDelay / 60).toFixed(2)} minutes)`}
       </span>
     </section>
   );
