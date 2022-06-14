647e8559d6d5212e265cd4f24d9b14ac0d9edec8
mintdart <96025197+mintdart@users.noreply.github.com>
2022-06-14 10:24:37 -0700

    fix network name in useNetworkProvider hook date when resolving network by query params

--- hooks/useNetworkProvider.tsx
+++ hooks/useNetworkProvider.tsx
@@ -15,27 +15,31 @@ export const useNetworkProvider = () => {
   let name: string | null = data?.chain?.name ?? null;
 
   if (pathname === '/streams' && !Number.isNaN(Number(query.chainId))) {
-    chainId = Number(query.chainId);
-    name = networkDetails[chainId]?.blockExplorerName;
+    const chain = chains.find((c) => c.id === Number(query.chainId));
+
+    if (chain) {
+      chainId = Number(chain.id);
+      name = chain.name;
+    }
   }
 
   if (pathname === '/salaries/[chain]/[address]' || pathname === '/vesting/[chain]/[address]') {
     const chainParam = query.chain;
 
-    const isParamChainId = Number.isNaN(Number(chainParam));
+    const isParamChainId = chainParam && !Number.isNaN(Number(chainParam));
 
     let chain = null;
 
     // handle routes like /salaries/ethereum/0x1234... & /salaries/1/0x1234
     if (isParamChainId) {
-      chain = typeof chainParam === 'string' && chains.find((c) => c.name.toLowerCase() === chainParam.toLowerCase());
-    } else {
       chain = typeof chainParam === 'string' && chains.find((c) => c.id === Number(chainParam));
+    } else {
+      chain = typeof chainParam === 'string' && chains.find((c) => c.name.toLowerCase() === chainParam.toLowerCase());
     }
 
     if (chain) {
       chainId = Number(chain.id);
-      name = networkDetails[chainId]?.blockExplorerName;
+      name = chain.name;
     }
   }
 
