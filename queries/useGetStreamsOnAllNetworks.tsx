def536350b5d2d5528c78795eeb919a0de0c8da1
mintdart <96025197+mintdart@users.noreply.github.com>
2022-09-05 13:30:31 +0700

    feat: show no.of active streams on a network in menu (#76)

---
+++ queries/useGetStreamsOnAllNetworks.tsx
@@ -0,0 +1,36 @@
+import { useAccount } from 'wagmi';
+import { useQuery } from 'react-query';
+import { useStreamAndHistoryQuery } from 'services/generated/graphql';
+import { chains, networkDetails } from 'utils/constants';
+
+async function fetchStreams(address?: string) {
+  try {
+    if (!address) return [];
+
+    const data = await Promise.all(
+      chains.map((chain) =>
+        useStreamAndHistoryQuery.fetcher(
+          {
+            endpoint: networkDetails[chain.id]?.subgraphEndpoint ?? '',
+          },
+          {
+            id: address?.toLowerCase() ?? '',
+            network: chain?.name ?? '',
+          }
+        )()
+      )
+    );
+
+    return chains.map((chain, index) => ({ id: chain.id, streams: data[index]?.user?.streams.length }));
+  } catch (error) {
+    return null;
+  }
+}
+
+const useGetStreamsOnAllNetworks = () => {
+  const [{ data: accountData }] = useAccount();
+
+  return useQuery(['allNetworkStreams', accountData?.address], () => fetchStreams(accountData?.address));
+};
+
+export default useGetStreamsOnAllNetworks;
