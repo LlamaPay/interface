2685ac7f197aaf13bf01b977add45f7c61986dc4
mintdart <96025197+mintdart@users.noreply.github.com>
2022-09-18 00:57:27 -0400

    update header line height

--- components/Layout/Hero/index.tsx
+++ components/Layout/Hero/index.tsx
@@ -7,7 +7,9 @@ import Image from 'next/image';
 export default function Hero({ onboardDialog }: { onboardDialog: DisclosureState }) {
   return (
     <div className="relative flex flex-col flex-wrap gap-8 bg-lp-secondary p-[30px] text-lp-gray-5 dark:bg-[linear-gradient(262.4deg,#434346_11.63%,#525A5A_100%)] dark:text-lp-white md:px-[30px] lg:min-h-[420px] lg:p-[60px] xl:min-h-[0px] xl:px-[120px] xl:py-[100px]">
-      <h1 className="font-exo z-10 max-w-[540px] text-5xl font-bold">Stream seamless recurring crypto payments!</h1>
+      <h1 className="font-exo z-10 max-w-[540px] text-5xl font-bold" style={{ lineHeight: '56px' }}>
+        Stream seamless recurring crypto payments!
+      </h1>
       <p className="z-10 w-fit max-w-[545px] text-lg backdrop-blur-xl">
         Automate salaries by streaming them - so employees can withdraw whenever they want.{' '}
       </p>
