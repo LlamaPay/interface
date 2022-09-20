f591fba38bcef7b511ac881f6948b3ba8ff586b1
mintdart <96025197+mintdart@users.noreply.github.com>
2022-09-20 18:26:11 -0400

    cleanup

--- components/Layout/Hero/Icons/Curve.tsx
+++ components/Layout/Hero/Icons/Curve.tsx
@@ -2,5 +2,10 @@ import Image from 'next/image';
 import curveLogo from './curve.png';
 
 export default function Curve() {
-  return <Image src={curveLogo} alt="curve finance" height={28} width={28} objectFit="contain" priority />;
+  return (
+    <span className="flex items-center gap-2 text-xl text-lp-gray-8 dark:text-lp-white">
+      <Image src={curveLogo} alt="curve finance" height={28} width={28} objectFit="contain" priority />
+      <p className="font-serif">Curve</p>
+    </span>
+  );
 }
--- components/Layout/Hero/index.tsx
+++ components/Layout/Hero/index.tsx
@@ -6,14 +6,15 @@ import heroGraphic from 'public/heroGraphic.svg';
 import Yearn from './Icons/Yearn';
 import Convex from './Icons/Convex';
 import Vector from './Icons/Vector';
-import BiFi from './Icons/Bifi';
+import Beefy from './Icons/Beefy';
 import Frax from './Icons/Frax';
 import Fantom from './Icons/Fantom';
 import DefiLlama from './Icons/DefiLlama';
 import Spookyswap from './Icons/Spookyswap';
 import Curve from './Icons/Curve';
+import classNames from 'classnames';
 
-export default function Hero({ onboardDialog }: { onboardDialog: DisclosureState }) {
+export default function Hero({ walletDialog }: { walletDialog: DisclosureState }) {
   return (
     <div className="relative isolate flex flex-col flex-wrap gap-5 overflow-clip bg-lp-secondary p-10 text-lp-gray-5 dark:bg-lp-green-4 dark:text-lp-white sm:p-20 xl:p-24">
       <h1 className="font-exo relative z-10 text-5xl font-bold leading-[3.5rem]">
@@ -31,17 +32,19 @@ export default function Hero({ onboardDialog }: { onboardDialog: DisclosureState
         <div className="3xl:max-w-full z-10 flex flex-wrap gap-8 text-lp-white lg:max-w-xl">
           {users.map((user) => (
             <Tooltip content={user.name} key={user.name} className="hover:cursor-default">
-              <div className="flex items-center">{user.logo}</div>
+              <div className={classNames('flex items-center', user.name === 'Curve Finance' && 'mr-[-10px]')}>
+                {user.logo}
+              </div>
             </Tooltip>
           ))}
         </div>
       </div>
 
       <button
-        className="primary-button z-10 w-full max-w-[26.25rem] border-none bg-lp-white text-lg font-semibold text-lp-primary dark:bg-lp-secondary dark:text-lp-black"
-        onClick={onboardDialog.toggle}
+        className="primary-button z-10 w-full max-w-[26.25rem] whitespace-pre-line border-none bg-lp-white text-lg font-semibold text-lp-primary dark:bg-lp-secondary dark:text-lp-black"
+        onClick={walletDialog.toggle}
       >
-        Start here
+        Connect your wallet to go to the app
       </button>
 
       <div className="hero-graphic">
@@ -57,6 +60,7 @@ interface IUser {
 }
 
 const users: Array<IUser> = [
+  { name: 'Beefy Finance', logo: <Beefy /> },
   {
     name: 'Convex Finance',
     logo: <Convex />,
@@ -80,5 +84,4 @@ const users: Array<IUser> = [
     name: 'Yearn Finance',
     logo: <Yearn />,
   },
-  { name: 'BiFi', logo: <BiFi /> },
 ];
