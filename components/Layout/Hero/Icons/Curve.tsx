9729c796e83c03944cbd23600c295f00cee9e9b1
mintdart <96025197+mintdart@users.noreply.github.com>
2022-09-20 20:24:37 -0400

    style: fix curve logo clipping on safari

--- components/Layout/Hero/Icons/Curve.tsx
+++ components/Layout/Hero/Icons/Curve.tsx
@@ -3,7 +3,7 @@ import curveLogo from './curve.png';
 
 export default function Curve() {
   return (
-    <span className="flex items-center gap-2 text-xl text-lp-gray-8 dark:text-lp-white">
+    <span className="flex min-w-[100px] items-center gap-2 text-xl text-lp-gray-8 dark:text-lp-white">
       <Image src={curveLogo} alt="curve finance" height={28} width={28} objectFit="contain" priority />
       <p className="font-serif">Curve</p>
     </span>
