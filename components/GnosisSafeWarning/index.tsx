94b3dcd1a9c15baecc86238db18ef50d56264408
nemusona <nemusonaneko@protonmail.com>
2022-10-10 15:06:06 -0500

    test

---
+++ components/GnosisSafeWarning/index.tsx
@@ -0,0 +1,19 @@
+import { useDialogState } from 'ariakit';
+import { FormDialog } from 'components/Dialog';
+import * as React from 'react';
+
+export default function GnosisSafeWarning() {
+  const dialog = useDialogState();
+
+  React.useEffect(() => {
+    if (process.env.NEXT_PUBLIC_SAFE === 'false' && window.location.ancestorOrigins[0] === 'https://gnosis-safe.io') {
+      dialog.show();
+    }
+  }, []);
+
+  return (
+    <FormDialog title="Switch to Gnosis App" dialog={dialog}>
+      <p>"idk what to put here"</p>
+    </FormDialog>
+  );
+}
