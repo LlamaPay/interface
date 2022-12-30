c3dfc72d85c03fe5e7be16d8756789ba152de31e
mintdart <96025197+mintdart@users.noreply.github.com>
2022-12-30 13:47:52 +0100

    add oracle, minPrice, setRedirects, cancelStreams actions

--- components/ScheduledTransfers/Payment.tsx
+++ components/ScheduledTransfers/Payment.tsx
@@ -1,10 +1,18 @@
+import * as React from 'react';
 import { PlusIcon } from '@heroicons/react/solid';
-import { DisclosureState } from 'ariakit';
+import { DisclosureState, useDialogState } from 'ariakit';
+import { FormDialog, TransactionDialog } from 'components/Dialog';
+import { InputText, SubmitButton } from 'components/Form';
+import { getAddress } from 'ethers/lib/utils';
 import { networkDetails } from 'lib/networkDetails';
 import { IScheduledTransferPayment } from 'queries/useGetScheduledTransfers';
+import { useQueryClient } from 'react-query';
+import { BeatLoader } from 'react-spinners';
 import { formatAddress } from 'utils/address';
-import { useNetwork } from 'wagmi';
+import { useContractWrite, useNetwork } from 'wagmi';
 import { formatFrequency } from './utils';
+import toast from 'react-hot-toast';
+import { scheduledPaymentsContractABI } from 'lib/abis/scheduledPaymentsContract';
 
 export function ScheduledTransferPayment({
   payments,
@@ -15,107 +23,267 @@ export function ScheduledTransferPayment({
   newPaymentDialog?: DisclosureState;
   isIncoming?: boolean;
 }) {
+  const txHash = React.useRef('');
+
+  const txDialogState = useDialogState();
+
   const [{ data: networkData }] = useNetwork();
 
   const explorerUrl = networkData?.chain?.id ? networkDetails[networkData.chain.id]?.blockExplorerURL : null;
 
   return (
-    <table className="w-full border-collapse">
-      <thead>
-        <tr>
-          <th className="whitespace-nowrap border border-llama-teal-2 py-[6px] px-4 text-center text-sm font-normal dark:border-lp-gray-7">
-            {isIncoming ? 'Payer' : 'Payee'}
-          </th>
-          <th className="whitespace-nowrap border border-llama-teal-2 py-[6px] px-4 text-center text-sm font-normal dark:border-lp-gray-7">
-            Start
-          </th>
-          <th className="whitespace-nowrap border border-llama-teal-2 py-[6px] px-4 text-center text-sm font-normal dark:border-lp-gray-7">
-            End
-          </th>
-          <th className="whitespace-nowrap border border-llama-teal-2 py-[6px] px-4 text-center text-sm font-normal dark:border-lp-gray-7">
-            Last Paid
-          </th>
-          <th className="whitespace-nowrap border border-llama-teal-2 py-[6px] px-4 text-center text-sm font-normal dark:border-lp-gray-7">
-            Amount USD
-          </th>
-          <th className="whitespace-nowrap border border-llama-teal-2 py-[6px] px-4 text-center text-sm font-normal dark:border-lp-gray-7">
-            Redirects
-          </th>
-          <th className="whitespace-nowrap border border-llama-teal-2 py-[6px] px-4 text-center text-sm font-normal dark:border-lp-gray-7">
-            Frequency
-          </th>
-        </tr>
-      </thead>
-      <tbody className="border border-llama-teal-2 dark:border-lp-gray-7">
-        {payments.map((payment) => (
-          <tr key={payment.id}>
-            <td className="table-description border border-solid border-llama-teal-2 text-center text-lp-gray-4 dark:border-lp-gray-7 dark:text-white">
-              {explorerUrl ? (
-                <a
-                  href={`${explorerUrl}/address/${isIncoming ? payment.pool.owner : payment.payee}`}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {formatAddress(isIncoming ? payment.pool.owner : payment.payee)}
-                </a>
-              ) : (
-                <>{formatAddress(isIncoming ? payment.pool.owner : payment.payee)}</>
-              )}
-            </td>
-            <td className="table-description border border-solid border-llama-teal-2 text-center text-lp-gray-4 dark:border-lp-gray-7 dark:text-white">
-              {payment.starts
-                ? new Date(Number(payment.starts) * 1000).toLocaleDateString() +
-                  ', ' +
-                  new Date(Number(payment.starts) * 1000).toLocaleTimeString()
-                : ''}
-            </td>
-            <td className="table-description border border-solid border-llama-teal-2 text-center text-lp-gray-4 dark:border-lp-gray-7 dark:text-white">
-              {payment.ends
-                ? new Date(Number(payment.ends) * 1000).toLocaleDateString() +
-                  ', ' +
-                  new Date(Number(payment.ends) * 1000).toLocaleTimeString()
-                : ''}
-            </td>
-            <td className="table-description border border-solid border-llama-teal-2 text-center text-lp-gray-4 dark:border-lp-gray-7 dark:text-white">
-              {payment.lastPaid !== payment.starts
-                ? new Date(Number(payment.lastPaid) * 1000).toLocaleDateString() +
-                  ', ' +
-                  new Date(Number(payment.lastPaid) * 1000).toLocaleTimeString()
-                : ''}
-            </td>
-            <td className="table-description border border-solid border-llama-teal-2 text-center text-lp-gray-4 dark:border-lp-gray-7 dark:text-white">
-              {payment.usdAmount
-                ? '$' +
-                  (Number(payment.usdAmount) / 1e8).toLocaleString(undefined, {
-                    maximumFractionDigits: 2,
-                  })
-                : ''}
-            </td>
-            <td className="table-description border border-solid border-llama-teal-2 text-center text-lp-gray-4 dark:border-lp-gray-7 dark:text-white">
-              {payment.redirects}
-            </td>
-            <td className="table-description border border-solid border-llama-teal-2 text-center text-lp-gray-4 dark:border-lp-gray-7 dark:text-white">
-              {formatFrequency(payment.frequency)}
-            </td>
-          </tr>
-        ))}
-        {newPaymentDialog && (
+    <>
+      <table className="w-full border-collapse">
+        <thead>
           <tr>
-            <td
-              className="table-description border border-solid border-llama-teal-2 py-4 text-center text-lp-gray-4 dark:border-lp-gray-7 dark:text-white"
-              colSpan={10}
-            >
-              <button
-                className="form-submit-button font-normalqq mx-auto flex max-w-fit flex-nowrap items-center gap-1"
-                onClick={newPaymentDialog.toggle}
-              >
-                <PlusIcon className="h-4 w-4" />
-                <span>New Payment</span>
-              </button>
-            </td>
+            <th className="whitespace-nowrap border border-llama-teal-2 py-[6px] px-4 text-center text-sm font-normal dark:border-lp-gray-7">
+              {isIncoming ? 'Payer' : 'Payee'}
+            </th>
+            <th className="whitespace-nowrap border border-llama-teal-2 py-[6px] px-4 text-center text-sm font-normal dark:border-lp-gray-7">
+              Start
+            </th>
+            <th className="whitespace-nowrap border border-llama-teal-2 py-[6px] px-4 text-center text-sm font-normal dark:border-lp-gray-7">
+              End
+            </th>
+            <th className="whitespace-nowrap border border-llama-teal-2 py-[6px] px-4 text-center text-sm font-normal dark:border-lp-gray-7">
+              Last Paid
+            </th>
+            <th className="whitespace-nowrap border border-llama-teal-2 py-[6px] px-4 text-center text-sm font-normal dark:border-lp-gray-7">
+              Amount USD
+            </th>
+            <th className="whitespace-nowrap border border-llama-teal-2 py-[6px] px-4 text-center text-sm font-normal dark:border-lp-gray-7">
+              Redirects
+            </th>
+            <th className="whitespace-nowrap border border-llama-teal-2 py-[6px] px-4 text-center text-sm font-normal dark:border-lp-gray-7">
+              Frequency
+            </th>
+
+            <th className="whitespace-nowrap border border-llama-teal-2 py-[6px] px-4 text-center text-sm font-normal dark:border-lp-gray-7"></th>
           </tr>
-        )}
-      </tbody>
-    </table>
+        </thead>
+
+        <tbody className="border border-llama-teal-2 dark:border-lp-gray-7">
+          {payments.map((payment) => (
+            <tr key={payment.id}>
+              <td className="table-description border border-solid border-llama-teal-2 text-center text-lp-gray-4 dark:border-lp-gray-7 dark:text-white">
+                {explorerUrl ? (
+                  <a
+                    href={`${explorerUrl}/address/${isIncoming ? payment.pool.owner : payment.payee}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {formatAddress(isIncoming ? payment.pool.owner : payment.payee)}
+                  </a>
+                ) : (
+                  <>{formatAddress(isIncoming ? payment.pool.owner : payment.payee)}</>
+                )}
+              </td>
+              <td className="table-description border border-solid border-llama-teal-2 text-center text-lp-gray-4 dark:border-lp-gray-7 dark:text-white">
+                {payment.starts
+                  ? new Date(Number(payment.starts) * 1000).toLocaleDateString() +
+                    ', ' +
+                    new Date(Number(payment.starts) * 1000).toLocaleTimeString()
+                  : ''}
+              </td>
+              <td className="table-description border border-solid border-llama-teal-2 text-center text-lp-gray-4 dark:border-lp-gray-7 dark:text-white">
+                {payment.ends
+                  ? new Date(Number(payment.ends) * 1000).toLocaleDateString() +
+                    ', ' +
+                    new Date(Number(payment.ends) * 1000).toLocaleTimeString()
+                  : ''}
+              </td>
+              <td className="table-description border border-solid border-llama-teal-2 text-center text-lp-gray-4 dark:border-lp-gray-7 dark:text-white">
+                {payment.lastPaid !== payment.starts
+                  ? new Date(Number(payment.lastPaid) * 1000).toLocaleDateString() +
+                    ', ' +
+                    new Date(Number(payment.lastPaid) * 1000).toLocaleTimeString()
+                  : ''}
+              </td>
+              <td className="table-description border border-solid border-llama-teal-2 text-center text-lp-gray-4 dark:border-lp-gray-7 dark:text-white">
+                {payment.usdAmount
+                  ? '$' +
+                    (Number(payment.usdAmount) / 1e8).toLocaleString(undefined, {
+                      maximumFractionDigits: 2,
+                    })
+                  : ''}
+              </td>
+              <td className="table-description border border-solid border-llama-teal-2 text-center text-lp-gray-4 dark:border-lp-gray-7 dark:text-white">
+                {explorerUrl && payment.redirects ? (
+                  <a href={`${explorerUrl}/address/${payment.redirects}`} target="_blank" rel="noopener noreferrer">
+                    {formatAddress(payment.redirects)}
+                  </a>
+                ) : (
+                  ''
+                )}
+              </td>
+              <td className="table-description border border-solid border-llama-teal-2 text-center text-lp-gray-4 dark:border-lp-gray-7 dark:text-white">
+                {formatFrequency(payment.frequency)}
+              </td>
+              {isIncoming ? (
+                <Redirects
+                  txHash={txHash}
+                  txDialogState={txDialogState}
+                  streamId={payment.streamId}
+                  poolContract={payment.pool.poolContract}
+                />
+              ) : (
+                <CancelTransfer
+                  txHash={txHash}
+                  txDialogState={txDialogState}
+                  streamId={payment.streamId}
+                  poolContract={payment.pool.poolContract}
+                />
+              )}
+            </tr>
+          ))}
+
+          {newPaymentDialog && (
+            <tr>
+              <td
+                className="table-description border border-solid border-llama-teal-2 py-4 text-center text-lp-gray-4 dark:border-lp-gray-7 dark:text-white"
+                colSpan={10}
+              >
+                <button
+                  className="form-submit-button font-normalqq mx-auto flex max-w-fit flex-nowrap items-center gap-1"
+                  onClick={newPaymentDialog.toggle}
+                >
+                  <PlusIcon className="h-4 w-4" />
+                  <span>New Payment</span>
+                </button>
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+
+      <TransactionDialog dialog={txDialogState} transactionHash={txHash.current || ''} />
+    </>
   );
 }
+
+const Redirects = ({
+  txHash,
+  txDialogState,
+  streamId,
+  poolContract,
+}: {
+  txHash: React.MutableRefObject<string>;
+  txDialogState: DisclosureState;
+  streamId: string;
+  poolContract: string;
+}) => {
+  const redirectDialog = useDialogState();
+
+  const queryClient = useQueryClient();
+
+  const [{ loading: updatingRedirect }, setRedirect] = useContractWrite(
+    {
+      addressOrName: poolContract,
+      contractInterface: scheduledPaymentsContractABI,
+    },
+    'setRedirect'
+  );
+
+  const updateRedirect = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const form = e.target as HTMLFormElement;
+
+    const newRedirects = form.newRedirects?.value;
+
+    setRedirect({ args: [streamId, getAddress(newRedirects)] }).then((res) => {
+      if (res.error) {
+        redirectDialog.hide();
+        toast.error(res.error.message);
+      } else {
+        const toastid = toast.loading(`Confirming Transaction`);
+
+        redirectDialog.hide();
+
+        txHash.current = res.data.hash;
+
+        txDialogState.toggle();
+
+        res.data?.wait().then((receipt) => {
+          toast.dismiss(toastid);
+
+          receipt.status === 1 ? toast.success('Transaction Success') : toast.error('Transaction Failed');
+        });
+
+        queryClient.invalidateQueries();
+      }
+    });
+  };
+
+  return (
+    <td className="table-description border border-solid border-llama-teal-2 text-center text-lp-gray-4 dark:border-lp-gray-7 dark:text-white">
+      <button className="primary-button py-1 px-[6px] text-xs font-medium" onClick={redirectDialog.toggle}>
+        Update Redirect
+      </button>
+
+      <FormDialog dialog={redirectDialog} title={'Redirects'}>
+        <span className="space-y-4 text-lp-gray-6 dark:text-white">
+          <form className="mx-auto flex flex-col gap-4" onSubmit={updateRedirect}>
+            <InputText name="newRedirects" label="New Redirect Address" placeholder="0x..." isRequired />
+
+            <SubmitButton className="mt-5">
+              {updatingRedirect ? <BeatLoader size={6} color="white" /> : 'Update'}
+            </SubmitButton>
+          </form>
+        </span>
+      </FormDialog>
+    </td>
+  );
+};
+
+const CancelTransfer = ({
+  txHash,
+  txDialogState,
+  streamId,
+  poolContract,
+}: {
+  txHash: React.MutableRefObject<string>;
+  txDialogState: DisclosureState;
+  streamId: string;
+  poolContract: string;
+}) => {
+  const queryClient = useQueryClient();
+
+  const [{}, cancelTransfer] = useContractWrite(
+    {
+      addressOrName: poolContract,
+      contractInterface: scheduledPaymentsContractABI,
+    },
+    'cancelTransfer'
+  );
+
+  const cancel = () => {
+    cancelTransfer({ args: [streamId] }).then((res) => {
+      if (res.error) {
+        toast.error(res.error.message);
+      } else {
+        const toastid = toast.loading(`Confirming Transaction`);
+
+        txHash.current = res.data.hash;
+
+        txDialogState.toggle();
+
+        res.data?.wait().then((receipt) => {
+          toast.dismiss(toastid);
+
+          receipt.status === 1 ? toast.success('Transaction Success') : toast.error('Transaction Failed');
+        });
+
+        queryClient.invalidateQueries();
+      }
+    });
+  };
+
+  return (
+    <td className="table-description border border-solid border-llama-teal-2 text-center text-lp-gray-4 dark:border-lp-gray-7 dark:text-white">
+      <button className="primary-button py-1 px-[6px] text-xs font-medium" onClick={cancel}>
+        Cancel
+      </button>
+    </td>
+  );
+};
--- components/ScheduledTransfers/Pool.tsx
+++ components/ScheduledTransfers/Pool.tsx
@@ -11,8 +11,12 @@ import { getFormattedMaxPrice, getMaxPriceInUSD } from './utils';
 import BigNumber from 'bignumber.js';
 import { ScheduledTransferPayment } from './Payment';
 import { scheduledPaymentsContractABI } from 'lib/abis/scheduledPaymentsContract';
+import { getAddress } from 'ethers/lib/utils';
+import toast from 'react-hot-toast';
+import { useQueryClient } from 'react-query';
+import { BeatLoader } from 'react-spinners';
 
-interface IFormElements {
+interface INewPaymentFormElements {
   toAddress: {
     value: string;
   };
@@ -32,6 +36,18 @@ interface IFormElements {
   };
 }
 
+interface IUpdateOracleFormElements {
+  newOracleAddress: {
+    value: string;
+  };
+}
+
+interface IUpdateMaxPriceFormElements {
+  newMinPrice: {
+    value: string;
+  };
+}
+
 export function ScheduledTransferPool({ pool }: { pool: IScheduledTransferPool }) {
   const [{ data: networkData }] = useNetwork();
 
@@ -43,6 +59,12 @@ export function ScheduledTransferPool({ pool }: { pool: IScheduledTransferPool }
 
   const newPaymentDialog = useDialogState();
 
+  const minPriceDialog = useDialogState();
+
+  const oracleDialog = useDialogState();
+
+  const queryClient = useQueryClient();
+
   const { mutateAsync } = useCreateScheduledTransferPayment({ poolAddress: pool.poolContract });
 
   const offset = new Date().getTimezoneOffset();
@@ -50,7 +72,7 @@ export function ScheduledTransferPool({ pool }: { pool: IScheduledTransferPool }
 
   const createScheduledPayment = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const form = e.target as HTMLFormElement & IFormElements;
+    const form = e.target as HTMLFormElement & INewPaymentFormElements;
     const toAddress = form.toAddress?.value;
     const usdAmount = form.usdAmount?.value;
     const paymentStartAt = form.paymentStartAt?.valueAsNumber;
@@ -81,7 +103,7 @@ export function ScheduledTransferPool({ pool }: { pool: IScheduledTransferPool }
     );
   };
 
-  const [{ loading }, setMaxPrice] = useContractWrite(
+  const [{ loading: updatingMinPrice }, setMinPrice] = useContractWrite(
     {
       addressOrName: pool.poolContract,
       contractInterface: scheduledPaymentsContractABI,
@@ -89,7 +111,79 @@ export function ScheduledTransferPool({ pool }: { pool: IScheduledTransferPool }
     'setMaxPrice'
   );
 
-  const formattedPrice = getFormattedMaxPrice(900, 18);
+  const [{ loading: updatingOracle }, changeOracle] = useContractWrite(
+    {
+      addressOrName: pool.poolContract,
+      contractInterface: scheduledPaymentsContractABI,
+    },
+    'changeOracle'
+  );
+
+  const updateMinPrice = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const form = e.target as HTMLFormElement & IUpdateMaxPriceFormElements;
+
+    const newMinPrice = form.newMinPrice?.value;
+
+    if (pool.token.decimals && newMinPrice) {
+      const formattedPrice = getFormattedMaxPrice(Number(newMinPrice), pool.token.decimals || 18);
+
+      setMinPrice({ args: [formattedPrice] }).then((res) => {
+        if (res.error) {
+          minPriceDialog.hide();
+          toast.error(res.error.message);
+        } else {
+          const toastid = toast.loading(`Confirming Transaction`);
+
+          minPriceDialog.hide();
+
+          txHash.current = res.data.hash;
+
+          txDialogState.toggle();
+
+          res.data?.wait().then((receipt) => {
+            toast.dismiss(toastid);
+
+            receipt.status === 1 ? toast.success('Transaction Success') : toast.error('Transaction Failed');
+          });
+
+          queryClient.invalidateQueries();
+        }
+      });
+    }
+  };
+
+  const updateOracle = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const form = e.target as HTMLFormElement & IUpdateOracleFormElements;
+
+    const newOracleAddress = form.newOracleAddress?.value;
+
+    changeOracle({ args: [getAddress(newOracleAddress)] }).then((res) => {
+      if (res.error) {
+        oracleDialog.hide();
+        toast.error(res.error.message);
+      } else {
+        const toastid = toast.loading(`Confirming Transaction`);
+
+        oracleDialog.hide();
+
+        txHash.current = res.data.hash;
+
+        txDialogState.toggle();
+
+        res.data?.wait().then((receipt) => {
+          toast.dismiss(toastid);
+
+          receipt.status === 1 ? toast.success('Transaction Success') : toast.error('Transaction Failed');
+        });
+
+        queryClient.invalidateQueries();
+      }
+    });
+  };
 
   return (
     <>
@@ -129,13 +223,20 @@ export function ScheduledTransferPool({ pool }: { pool: IScheduledTransferPool }
                 Oracle
               </th>
               <td className="table-description border border-solid border-llama-teal-2 text-lp-gray-4 dark:border-lp-gray-7 dark:text-white">
-                {explorerUrl ? (
-                  <a href={`${explorerUrl}/address/${pool.oracle}`} target="_blank" rel="noopener noreferrer">
-                    {pool.oracle}
-                  </a>
-                ) : (
-                  <>{pool.oracle}</>
-                )}
+                <span className="flex flex-wrap items-center justify-between gap-4">
+                  <span>
+                    {explorerUrl ? (
+                      <a href={`${explorerUrl}/address/${pool.oracle}`} target="_blank" rel="noopener noreferrer">
+                        {pool.oracle}
+                      </a>
+                    ) : (
+                      <>{pool.oracle}</>
+                    )}
+                  </span>
+                  <button className="primary-button py-1 px-[6px] text-xs font-medium" onClick={oracleDialog.toggle}>
+                    Change
+                  </button>
+                </span>
               </td>
             </tr>
             <tr>
@@ -145,11 +246,19 @@ export function ScheduledTransferPool({ pool }: { pool: IScheduledTransferPool }
                 </Tooltip>
               </th>
               <td className="table-description border border-solid border-llama-teal-2 text-lp-gray-4 dark:border-lp-gray-7 dark:text-white">
-                {pool.maxPrice
-                  ? `$${getMaxPriceInUSD(pool.maxPrice, pool.token.decimals || 18).toLocaleString(undefined, {
-                      maximumFractionDigits: 2,
-                    })}`
-                  : ''}
+                <span className="flex flex-wrap items-center justify-between gap-4">
+                  <span>
+                    {pool.maxPrice
+                      ? `$${getMaxPriceInUSD(pool.maxPrice, pool.token.decimals || 18).toLocaleString(undefined, {
+                          maximumFractionDigits: 2,
+                        })}`
+                      : ''}
+                  </span>
+
+                  <button className="primary-button py-1 px-[6px] text-xs font-medium" onClick={minPriceDialog.toggle}>
+                    Change
+                  </button>
+                </span>
               </td>
             </tr>
             <tr>
@@ -184,6 +293,30 @@ export function ScheduledTransferPool({ pool }: { pool: IScheduledTransferPool }
             <InputText name="frequency" label="Frequency (Days)" placeholder="1" isRequired />
 
             <SubmitButton className="mt-5">Create</SubmitButton>
+          </form>
+        </span>
+      </FormDialog>
+
+      <FormDialog dialog={minPriceDialog} title={'Min Price'}>
+        <span className="space-y-4 text-lp-gray-6 dark:text-white">
+          <form className="mx-auto flex flex-col gap-4" onSubmit={updateMinPrice}>
+            <InputAmount name="newMinPrice" label="New Minimum Price (USD)" placeholder="1000" isRequired />
+
+            <SubmitButton className="mt-5">
+              {updatingMinPrice ? <BeatLoader size={6} color="white" /> : 'Update'}
+            </SubmitButton>
+          </form>
+        </span>
+      </FormDialog>
+
+      <FormDialog dialog={oracleDialog} title={'Oracle'}>
+        <span className="space-y-4 text-lp-gray-6 dark:text-white">
+          <form className="mx-auto flex flex-col gap-4" onSubmit={updateOracle}>
+            <InputText name="newOracleAddress" label="New Address" placeholder="0x..." isRequired />
+
+            <SubmitButton className="mt-5">
+              {updatingOracle ? <BeatLoader size={6} color="white" /> : 'Update'}
+            </SubmitButton>
           </form>
         </span>
       </FormDialog>
