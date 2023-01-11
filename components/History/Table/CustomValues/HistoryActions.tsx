a315018423d318160f8fe0a750d46e4aa0663da1
nemusona <nemusonaneko@protonmail.com>
2023-01-11 04:32:48 -0600

    add etherscan link to history

--- components/History/Table/CustomValues/HistoryActions.tsx
+++ components/History/Table/CustomValues/HistoryActions.tsx
@@ -3,17 +3,26 @@ import { useTranslations } from 'next-intl';
 import type { IHistory } from '~/types';
 import { MoreInfo } from './MoreInfo';
 import { WithdrawInvoice } from './WithdrawInvoice';
+import { useNetworkProvider } from '~/hooks';
+import { networkDetails } from '~/lib/networkDetails';
 
 export const HistoryActions = ({ data }: { data: IHistory }) => {
   const dialog = useDialogState();
-
+  const { chainId } = useNetworkProvider();
   const t = useTranslations('History');
-
-  if (data.eventType === 'Deposit') return null;
 
   return (
     <>
-      <span className="flex">
+      <span className="flex space-x-2">
+        {chainId && (
+          <button className=" row-action-links ml-auto">
+            <a
+              target="_blank"
+              rel="noreferrer noopener"
+              href={`${networkDetails[chainId].blockExplorerURL}tx/${data.txHash}`}
+            >{`${networkDetails[chainId].blockExplorerName}`}</a>
+          </button>
+        )}
         {data.eventType === 'Withdraw' ? (
           <WithdrawInvoice data={data} />
         ) : (
