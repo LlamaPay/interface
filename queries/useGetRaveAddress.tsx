6fc6504d59711311419ae2875fb22cb2131d92a5
nemusona <nemusonaneko@protonmail.com>
2023-02-03 18:22:55 -0600

    readd rave names

--- queries/useGetRaveAddress.tsx
+++ queries/useGetRaveAddress.tsx
@@ -4,10 +4,13 @@ import raveContract from '~/lib/abis/raveContract';
 import { RAVE_RESOLVER } from '~/lib/contracts';
 import { networkDetails } from '~/lib/networkDetails';
 
+const zero = '0x98FEF8Da2e27984092B00D8d351b1e625B7E0492'.toLowerCase();
+
 export async function getRaveAddress(domain: string) {
   try {
     const rave = new ethers.Contract(getAddress(RAVE_RESOLVER), raveContract, networkDetails[250].chainProviders);
-    return await rave.getOwner(domain);
+    const owner = await rave.getOwner(domain);
+    return owner.toLowerCase() === zero ? null : owner;
   } catch (error) {
     return null;
   }
