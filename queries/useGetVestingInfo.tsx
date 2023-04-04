ff2ec1277264853d67aa828fb79d3e39fedf83bd
nemusona <nemusonaneko@protonmail.com>
2023-04-04 17:52:37 -0500

    just rug refetch by 1h

--- queries/useGetVestingInfo.tsx
+++ queries/useGetVestingInfo.tsx
@@ -277,6 +277,6 @@ interface IVestingInfoByQueryParams {
 
 export function useGetVestingInfoByQueryParams({ address, chainId, provider }: IVestingInfoByQueryParams) {
   return useQuery(['vestingInfo', address, chainId], () => getVestingInfo(address, provider, chainId), {
-    refetchInterval: false,
+    refetchInterval: 3600000,
   });
 }
