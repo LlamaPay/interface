a938e1ba2a11fce2c8939a383751f27f8531f5b6
mintdart <96025197+mintdart@users.noreply.github.com>
2023-04-04 20:39:15 -0500

    refactor usePayer query

--- queries/usePayers.tsx
+++ queries/usePayers.tsx
@@ -6,32 +6,44 @@ import type { IPayer, IToken } from '~/types';
 import BigNumber from 'bignumber.js';
 import { createERC20Contract } from '~/utils/tokenUtils';
 import { getAddress } from 'ethers/lib/utils';
+import { Multicall } from 'ethereum-multicall';
+import { llamaContractABI } from '~/lib/abis/llamaContract';
 
 const fetchPayers = async (userAddress: string, tokens: IToken[] | null, provider: Provider | null) => {
   if (!userAddress || userAddress === '' || !tokens || tokens.length < 1 || !provider) return null;
   try {
-    const res = await Promise.allSettled(tokens.map((c) => c.llamaTokenContract.payers(userAddress)));
+    const multicall = new Multicall({ ethersProvider: provider, tryAggregate: true });
 
-    const data: IPayer[] = res.map((d, index) => {
-      const amount = (d.status === 'fulfilled' && new BigNumber(d.value?.totalPaidPerSec?.toString())) ?? null;
+    const res2 = await multicall.call(
+      tokens.map((token) => ({
+        reference: token.llamaTokenContract.address,
+        contractAddress: token.llamaTokenContract.address,
+        abi: llamaContractABI,
+        calls: [{ reference: 'payers', methodName: 'payers', methodParameters: [getAddress(userAddress)] }],
+      }))
+    );
 
-      const lastPayerUpdate = d.status === 'fulfilled' ? d.value?.lastPayerUpdate ?? null : null;
+    const data = tokens.map((token) => {
+      const values = res2.results[getAddress(token.llamaTokenContract.address)]?.callsReturnContext?.[0]?.returnValues;
+
+      const lastPayerUpdate = values[0];
+      const totalPaidPerSec = values[1]?.hex ? new BigNumber(values[1].hex).toFixed(5) : '';
 
       return {
-        name: tokens[index]?.name,
-        address: tokens[index]?.tokenAddress,
-        symbol: tokens[index]?.symbol,
-        contractAddress: tokens[index]?.llamaContractAddress,
-        tokenDecimals: tokens[index].decimals,
-        tokenContract: createERC20Contract({ tokenAddress: getAddress(tokens[index]?.tokenAddress), provider }),
-        totalPaidPerSec: amount ? amount.toFixed(5) : '',
+        name: token.name,
+        address: token.tokenAddress,
+        symbol: token.symbol,
+        contractAddress: token.llamaContractAddress,
+        tokenDecimals: token.decimals,
+        tokenContract: createERC20Contract({ tokenAddress: getAddress(token.tokenAddress), provider }),
+        totalPaidPerSec: totalPaidPerSec,
         lastPayerUpdate,
       };
     });
 
     return data;
   } catch (error) {
-    // console.log(error);
+    console.log(error);
     return null;
   }
 };
