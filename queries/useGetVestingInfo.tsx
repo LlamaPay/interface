b935649d72d58b7ff5eb97b97349aa20b103c7a6
0xngmi <0xngmi@protonmail.com>
2024-04-28 22:06:41 +0100

    fixes

--- queries/useGetVestingInfo.tsx
+++ queries/useGetVestingInfo.tsx
@@ -49,9 +49,7 @@ const vestingEscrowCallsSubgraph = [
 ];
 
 const subgraphs: { [key: number]: string } = {
-  // 1: 'https://api.thegraph.com/subgraphs/name/nemusonaneko/llamapay-vesting-mainnet',
-  5: 'https://api.thegraph.com/subgraphs/name/nemusonaneko/llamapay-vesting-goerli',
-  42161: 'https://api.thegraph.com/subgraphs/name/nemusonaneko/llamapay-vesting-arbitrum',
+  1: 'https://api.studio.thegraph.com/query/73158/vesting-ethereum/version/latest',
 };
 
 const multicalls: { [key: number]: string } = {
@@ -195,7 +193,9 @@ async function getVestingInfo(userAddress: string | undefined, provider: Provide
       {
         vestingEscrows(where: {admin: "${userAddress.toLowerCase()}"}, first:1000) {
           id
-          factory
+          factory {
+            id
+          }
           admin
           recipient
           token {
@@ -220,7 +220,9 @@ async function getVestingInfo(userAddress: string | undefined, provider: Provide
       {
         vestingEscrows(where: {recipient: "${userAddress.toLowerCase()}"}) {
           id
-          factory
+          factory {
+            id
+          }
           admin
           recipient
           token {
@@ -260,7 +262,7 @@ async function getVestingInfo(userAddress: string | undefined, provider: Provide
       for (const i in escrows) {
         const returns = vestingContractInfoResults[escrows[i].id].callsReturnContext;
         const result = {
-          factory: escrows[i].factory ?? networkDetails[chainId].vestingFactory,
+          factory: escrows[i].factory.id ?? networkDetails[chainId].vestingFactory,
           contract: escrows[i].id.toString(),
           unclaimed: new BigNumber(returns[0].returnValues[0].hex).toString(),
           locked: new BigNumber(returns[1].returnValues[0].hex).toString(),
