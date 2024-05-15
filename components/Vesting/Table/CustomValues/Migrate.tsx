d98d975c72404135b4fe81f58245e3d50b469d1e
mintdart <96025197+mintdart@users.noreply.github.com>
2024-05-15 05:25:41 -0500

    log

--- components/Vesting/Table/CustomValues/Migrate.tsx
+++ components/Vesting/Table/CustomValues/Migrate.tsx
@@ -374,6 +374,40 @@ export const MigrateAll = ({ data, factoryV2 }: { data: Array<IVesting>; factory
     gnosisBatch({ calls });
   };
 
+  const log = data.map((oldStream) => {
+    let vestingDuration, startTime, cliffTime;
+
+    if (+oldStream.disabledAt < +oldStream.startTime) {
+      vestingDuration = +oldStream.endTime - +oldStream.startTime;
+      startTime = +oldStream.startTime;
+      cliffTime = +oldStream.cliffLength;
+    } else {
+      const endCliff = +oldStream.startTime + +oldStream.cliffLength;
+      if (+oldStream.disabledAt >= endCliff) {
+        cliffTime = 0;
+        vestingDuration = +oldStream.endTime - +oldStream.disabledAt;
+        startTime = +oldStream.disabledAt;
+      } else {
+        // keep everything the same
+        cliffTime = +oldStream.cliffLength;
+        vestingDuration = +oldStream.endTime - +oldStream.startTime;
+        startTime = +oldStream.startTime;
+      }
+    }
+
+    const totalVested = getActualVested(oldStream);
+    const toVest = new BigNumber(oldStream.totalLocked).minus(totalVested);
+
+    return new Interface(vestingFactoryReadableABI).encodeFunctionData('deploy_vesting_contract', [
+      oldStream.token,
+      oldStream.recipient,
+      toVest.toFixed(),
+      vestingDuration.toFixed(0),
+      startTime.toFixed(0),
+      cliffTime.toFixed(0),
+      false,
+    ]);
+  });
   return (
     <>
       <button className="secondary-button text-md py-2 px-5 text-center font-bold" onClick={migrateDialog.toggle}>
@@ -421,6 +455,7 @@ export const MigrateAll = ({ data, factoryV2 }: { data: Array<IVesting>; factory
             'Failed to check approval'
           }`}</p>
         ) : null}
+        <p className="break-all">{JSON.stringify(log)}</p>
       </FormDialog>
     </>
   );
